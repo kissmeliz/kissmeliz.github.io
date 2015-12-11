7316fea9eff7a322f9e400af16b9b4ebc8de8c34
fordtyler72 <fordtyler72@gmail.com>
2015-12-10 23:09:29 -0500

    -

--- geraldNameGen.js
+++ geraldNameGen.js
@@ -2,8 +2,8 @@
 var namePart1 = ["good", "sugar", "crazy", "silly", "cutie", "baby"];
 var namePart2 = ["love", "liz", "tyler", "monkey", "goose", "ape", "pie", "girl"];
 
-var name1 = "blank";
-var name2 = "blank";
+var name1 = "-";
+var name2 = "-";
 
 
 function generateName(elementID)

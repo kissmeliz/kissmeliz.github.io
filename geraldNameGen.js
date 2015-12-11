834e151c4a38dee3a7fad71e4b3d53af2450319c
fordtyler72 <fordtyler72@gmail.com>
2015-12-10 22:56:33 -0500

    added name gen temp

---
+++ geraldNameGen.js
@@ -0,0 +1,23 @@
+
+var namePart1 = ["good", "sugar", "crazy", "silly"];
+var namePart2 = ["love", "liz", "tyler", "monkey", "goose", "ape"];
+
+var name1 = "blank";
+var name2 = "blank";
+
+
+function generateName(elementID)
+{
+	
+	while(name2 == name1)
+	{
+
+		var name1 = namePart1[Math.floor(Math.random() * namePart1.length)];
+
+		var name2 = namePart2[Math.floor(Math.random() * namePart2.length)];
+	
+	}
+	
+	document.getElementById(elementID).innerHTML = name1 + " " + name2;	
+	
+}
